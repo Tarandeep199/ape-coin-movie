addb8047065115ad113602296bf9b55c6706d027
Tarandeep <tarandeepsingh199@gmail.com>
2022-03-31 22:32:25 -0700

    apecoin movie

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import ape from '../public/images/APECOINMOVIE.jpg'
 export default function Home() {
   return (
     <div className='flex flex-col items-center justify-center w-full h-screen'>
-      <h1 className='pb-8 text-3xl'>APEPCOIN MOVIE</h1>
+      <h1 className='pb-8 text-5xl'>APEPCOIN MOVIE</h1>
        
       <div className= 'relative w-32 h-32 pt-6 overflow-hidden rounded-full ring-2 ring-pink-300 ring-offset-44'>
         <Image
